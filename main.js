a08973fdcbe3086d445cc533924a12cc3199eeb6
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-08 19:17:38 +0530

    impl

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement this function
+  // Extract request details
+  const { method, url, headers } = req;
+
+  // Create response object
+  const responseObject = {
+    method,
+    url,
+    headers,
+  };
+
+  // Set response headers
+  res.setHeader("Content-Type", "application/json");
+
+  // Handle different HTTP methods
+  if (req.method === "GET" || req.method === "POST") {
+    // Send response with request details
+    res.statusCode = 200;
+    res.end(JSON.stringify(responseObject));
+  } else {
+    // Send 405 Method Not Allowed for unsupported methods
+    res.statusCode = 405;
+    res.end(JSON.stringify({ error: "Method Not Allowed" }));
+  }
 });
 
 server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
 
 module.exports = { server };
